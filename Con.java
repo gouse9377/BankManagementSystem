a0ac34d306f9c6220d93bb6804a101368788b2d4
Shaik Gouse Mohiddin <gousemohiddin9377@gmail.com>
2025-04-28 22:23:06 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,15 @@
+package bank.managaement.system;
+import java.sql.*;
+public class Con {
+    Connection connection;
+    Statement statement;
+
+    public Con(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","root");
+            statement = connection.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
